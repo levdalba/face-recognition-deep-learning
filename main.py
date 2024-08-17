b68390b2c9817dee3c172733d5173a7e782d8ef0
Levan Dalbashvili <levandalbashvili9@gmail.com>
2024-08-17 13:43:47 +0400

    making predictions

--- main.py
+++ main.py
@@ -271,3 +271,57 @@ EPOCHS = 50
 # train(train_data, EPOCHS)
 
 
+from tensorflow.keras.metrics import Precision, Recall
+
+# Get a batch of test data
+test_input, test_val, y_true = test_data.as_numpy_iterator().next()
+y_hat = siamese_model.predict([test_input, test_val])
+
+# Post processing the results
+y_pred = [1 if prediction[0] > 0.5 else 0 for prediction in y_hat]
+
+
+print(y_true)
+print(y_pred)
+
+# Calculate metrics
+# Creating a metric object
+m = Recall()
+
+# Calculating the recall value
+m.update_state(y_true, y_hat)
+
+# Return Recall Result
+m.result().numpy()
+# Creating a metric object
+m = Precision()
+
+# Calculating the recall value
+m.update_state(y_true, y_hat)
+
+# Return Recall Result
+m.result().numpy()
+
+r = Recall()
+p = Precision()
+
+for test_input, test_val, y_true in test_data.as_numpy_iterator():
+    yhat = siamese_model.predict([test_input, test_val])
+    r.update_state(y_true, yhat)
+    p.update_state(y_true, yhat)
+
+print(r.result().numpy(), p.result().numpy())
+
+# Set plot size
+plt.figure(figsize=(10, 8))
+
+# Set first subplot
+plt.subplot(1, 2, 1)
+plt.imshow(test_input[0])
+
+# Set second subplot
+plt.subplot(1, 2, 2)
+plt.imshow(test_val[0])
+
+# Renders cleanly
+plt.show()
